4d539a6585c205c699fb3b61c62d843f17948a77
jrubendiaz <jj.rubendiaz@gmail.com>
2017-12-07 14:24:06 +0000

    Add some visual improvements
    Improvements in the views of contactList
    
    CSS -> contact-item & add-new-user-content was updated
    HTML -> improve the contact-list grid

--- js/controllers/contacts.controller.js
+++ js/controllers/contacts.controller.js
@@ -11,15 +11,16 @@
 
         // Own Variables
         vm.contacts = {};
-        vm.edit = 'personal';
+        vm.edit = 'init';
+
         vm.newContact = {
-            name: "",
-            email: "",
-            phone: "",
-            photo: "",
+            name: "", // update by name input
+            email: "", // update by email input
+            phone: "", // update by phone input
+            photo: "", // update by photo input
             favoriteList: {
-                marvel:[],
-                giphy: []
+                marvel:[], //update by marvelManagerComponent
+                giphy: [] //update by giphyManagerComponent
             }
         }
 
@@ -31,19 +32,27 @@
         ////////////////
 
         function activate() {
+            //load users/contacts from ContactProvider (localStore)
             vm.contacts = ContactProvider.getAll();
-            console.log(vm.contacts);
+        }
+        function resetNewContact() {
+            vm.newContact = {
+                name: "", // update by name input
+                email: "", // update by email input
+                phone: "", // update by phone input
+                photo: "", // update by photo input
+                favoriteList: {
+                    marvel:[], //update by marvelManagerComponent
+                    giphy: [] //update by giphyManagerComponent
+                }
+            }
         }
         function addContact() {
-            console.log(vm.newContact);
-            let contact = {
-                name : vm.name || "Nombre aleatorio",
-                email : vm.email || "aleatorio@correo.es",
-                photo : vm.photo || "https://images.unsplash.com/photo-1485217988980-11786ced9454?auto=format&fit=crop&w=750&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D",
-                id : Date.now()
-            }
+            //add user/contact to localStore using ContactProvider add method
             ContactProvider.add(vm.newContact);
+            //reload users/contacts from localStore using ContactProvider methods
             vm.contacts = ContactProvider.getAll();
+            resetNewContact();
         }
     }
 })();
